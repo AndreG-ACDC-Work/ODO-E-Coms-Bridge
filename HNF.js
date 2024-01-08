f0a026cf77362f36ca16edeb3050a422928f01e9
AndreG-ACDC-Work <andreg@acdc.co.za>
2024-01-08 07:40:58 +0200

    Potato

--- HNF.js
+++ HNF.js
@@ -1,11 +1,11 @@
 function loadHeaderAndFooter() {
-    fetch('HNF/Header.html')
+    fetch('../HNF/Header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('Header').innerHTML = data;
         });
 
-    fetch('HNF/Footer.html')
+    fetch('../HNF/Footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('Footer').innerHTML = data;
